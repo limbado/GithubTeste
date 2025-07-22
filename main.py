f3a89e65f05bbe81656d58ce7a4c0efc3433038b
limbado <limbado23@gmail.com>
2025-07-22 13:22:17 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import time
+
+def calculadora(num1: float, num2: float, operador: str) -> float:
+    """
+    Usar nan como valor inicial é uma boa prática. 
+    Se o operador fornecido não corresponder a nenhuma das opções válidas (+, -, etc.), a função retornará nan, 
+    sinalizando que o cálculo não pôde ser realizado.
+    """
+    result = float("nan")
+    if operador == '+':
+        result = num1 + num2
+    elif operador == '-':
+        result = num1 - num2
+
+    return result
+
+
+if __name__ == "__main__":
+
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        try:
+            print('Calculadora')
+            print('----------------------------------\n')
+
+
+        except ValueError:
+            print('Dados inválidos! -> Tente novamente!')
+            time.sleep(2)
+
+        except ZeroDivisionError:
+            print('Impossível dividir por zero! -> Tente novamente!')
+            time.sleep(2)
+
+    print('\nVolte sempre!\n')
